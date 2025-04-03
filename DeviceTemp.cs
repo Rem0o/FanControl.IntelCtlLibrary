1c65a1350fd8b48e8697ffdc1c8d7de90bf375e0
Rem0o <remi.mercier.software@gmail.com>
2025-04-03 13:56:31 -0400

    Update DeviceTemp.cs
    use better descriptive string instead of type.ToString()

--- DeviceTemp.cs
+++ DeviceTemp.cs
@@ -14,7 +14,7 @@ namespace FanControl.IntelCtlLibraryPlugin
             using (var properties = new ctl_temp_properties_t())
             {
                 CtlLibrary.ctlTemperatureGetProperties(handle, properties);
-                Name = properties.type.ToString();
+                Name = TypeToString(properties.type);
             }
 
             _handle = handle;
@@ -32,6 +32,21 @@ namespace FanControl.IntelCtlLibraryPlugin
         public void Dispose()
         {
             CtlLibrary.delete_double_Ptr(doublePtr);
+        }
+
+        private static string TypeToString(ctl_temp_sensors_t type)
+        {
+            return type switch
+            {
+                ctl_temp_sensors_t.CTL_TEMP_SENSORS_GLOBAL => "Global",
+                ctl_temp_sensors_t.CTL_TEMP_SENSORS_GPU => "GPU",
+                ctl_temp_sensors_t.CTL_TEMP_SENSORS_MEMORY => "Memory",
+                ctl_temp_sensors_t.CTL_TEMP_SENSORS_GLOBAL_MIN => "Global Min",
+                ctl_temp_sensors_t.CTL_TEMP_SENSORS_GPU_MIN => "GPU Min",
+                ctl_temp_sensors_t.CTL_TEMP_SENSORS_MEMORY_MIN => "Memory Min",
+                ctl_temp_sensors_t.CTL_TEMP_SENSORS_MAX => "Max",
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+            };
         }
     }
 }
